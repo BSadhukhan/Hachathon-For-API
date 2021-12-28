da5d7d67a575e6219bb5b7e68ac8666d6c972879
Mpate <Mpate@DESKTOP-2VU7Q5U>
2021-12-28 18:13:59 -0500

    LmsApiApplicationTest_Updated

--- src/test/java/com/ninja/lms/LmsApiApplicationTests.java
+++ src/test/java/com/ninja/lms/LmsApiApplicationTests.java
@@ -26,31 +26,10 @@ class LmsApiApplicationTests {
 	@Autowired
 	UserService userService;
 	
-	@MockBean
-	UserRepository userRepo;
-	
 	@Test
-	public void getAllUserTest() {
+	public void contextLoads() {
 		
-		Date utilDate = new Date();
-		
-		UserSkillMap userSkill_1 = null;
-		
-		//String userId, String userFirstName, String userLastName, long userPhoneNumber, String userLocation, String userTimeZone, 
-		//String userLinkedinUrl, String userEduUg, String userEduPg, String userComments, String userVisaStatus, Timestamp creationTime, 
-		//Timestamp lastModTime, Set<UserSkillMap> userSkillMapSet 
-		
-		User user = new User("U12", "Baisali", "Sadhukhan", 1111111111, "Pittsburgh", "EST", "jhsgf", "B.Tech", "PG", "nbdjhgd" , "H4", 
-				new Timestamp(utilDate.getTime()), new Timestamp(utilDate.getTime()), new HashSet<UserSkillMap>());
-				
-		//int skillId, String skillName, UserSkillMap userSkill, Timestamp creationTime, Timestamp lastModTime		
-		Skill skill = new Skill(110, "JavaSpring", userSkill_1, new Timestamp(utilDate.getTime()), new Timestamp(utilDate.getTime()));
-		
-		//String userSkillId, User user, Skill skill, int monthsOfExp, Timestamp creationTime, Timestamp lastModTime
-		userSkill_1 = new UserSkillMap("US11", user, skill, 12, new Timestamp(utilDate.getTime()), new Timestamp(utilDate.getTime()));
-		
-		when(userRepo.findAll()).thenReturn(Stream.of(user).collect(Collectors.toList()));
-		assertEquals(1, userService.getAllUsers().size());
 	}
+
 
 }
